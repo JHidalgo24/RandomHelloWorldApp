4f7de445d17fe5f3bcec4ee20d82908deb038634
Justin <justinh8100@gmail.com>
2022-10-18 23:33:00 -0500

    project commit

---
+++ HelloWorldApp/HelloWorldApp/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloWorldApp
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        private readonly string[] _helloList = {"Hello World","Hola Mundo","Bonjour le monde","Hallo Welt","Ciao mondo", "你好世界", "Olá Mundo","こんにちは世界"};
+        private readonly string[] _colors = {"Red", "Teal", "Blue","Yellow","Purple","Aqua", "Lime", "pink"};
+        private int _greetingNum = 1;
+        private Random random = new Random();
+        public MainPage()
+        {
+            InitializeComponent();
+            GreetingLabel.Text = _helloList[_greetingNum];
+            Color color = Color.FromRgb(random.Next(256), random.Next(256), random.Next(256));
+        }
+        void Handle_Greeting(object sender, System.EventArgs e)
+        {
+            
+            var num = random.Next(0, 8);
+            if (_greetingNum == num)
+            {
+                switch (_greetingNum)
+                {
+                    //prevents it from being helloList[7] twice
+                    case 7:
+                        num--;
+                        break;
+                    //prevents it from being helloList[0] twice
+                    case 0:
+                        num++;
+                        break;
+                    //prevents it from being the same number twice and either goes up or down randomly
+                    default:
+                    {
+                        if ((num + random.Next(0,1000)) % 2 == 0)
+                            num--;
+                        else
+                            num++;
+                        break;
+                    }
+                }
+            }
+            
+            
+            _greetingNum = num;
+            CurrentGreeting.Text = $"Greeting {num + 1} of {_helloList.Length}";
+            GreetingLabel.Text = _helloList[_greetingNum];
+            Color color = Color.FromRgb(random.Next(256), random.Next(256), random.Next(256));
+
+            GreetingLabel.TextColor = color;
+        }
+        
+
+    }
+}
